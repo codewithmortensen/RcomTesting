1c462b6987d7888650d3fa9051af7ab03e71ab52
Mortenen Ulysse <codewithmortensen@gmail.com>
2023-12-05 09:10:23 -0500

    create the attendance Record type

--- app/types/definitions.ts
+++ app/types/definitions.ts
@@ -52,6 +52,18 @@ export type AttendanceTable = {
   work_day_status: WorkDayStatus;
 };
 
+export type EmployeeAttendance = {
+  employee_id: number;
+  first_name: string;
+  last_name: string;
+  attendance_date: string;
+  check_in_time: string;
+  check_out_time: string;
+  check_in_status: CheckInStatus;
+  check_out_status: CheckOutStatus;
+  work_day_status: WorkDayStatus;
+};
+
 export type EmployeeProfile = {
   employee_id: number;
   profile_id: number;
@@ -70,4 +82,16 @@ export type EmployeeProfile = {
   start_shift: string;
   end_shift: string;
   reports_to?: number;
+};
+
+export type AttendanceReport = {
+  employee_id: number;
+  first_name: string;
+  last_name: string;
+  on_time_check_in_count: number;
+  late_check_in_count: number;
+  on_time_check_out_count: number;
+  early_check_out_count: number;
+  absent_count: number;
+  present_count: number;
 };
